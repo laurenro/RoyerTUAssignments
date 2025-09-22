178e2cd9ebc09d29b686c2e9c57dd1c7e9a05a7f
laurenro <114772568+laurenro@users.noreply.github.com>
2025-09-22 13:30:35 -0400

    results added and comments for tmp file removed

--- HumanEvolutionWk1.py
+++ HumanEvolutionWk1.py
@@ -1,14 +1,13 @@
 # RoyerTUAssignments
 import allel
 import pandas as pd
-import collections #import Counter
-import numpy as np
+import collections
 import matplotlib.pyplot as plt
 
-tmpvcf = 'C:/Users/laure/Documents/BIOL5131/Altai_1000lines.vcf'
-# longvcf = 'C:/Users/laure/Documents/BIOL5131/AltaiNea.hg19_1000g.22.mod.vcf.gz' ##update to just 1000lines to run from GitHub repo
+# tmpvcf = 'Altai_1000lines.vcf'
+longvcf = 'C:/Users/laure/Documents/BIOL5131/AltaiNea.hg19_1000g.22.mod.vcf.gz' ##update to just 1000lines to run from GitHub repo
 
-# readvcf = allel.read_vcf(tmpvcf)
+# readvcf = allel.read_vcf(longvcf)
 # keys = readvcf.keys()
 # print(keys) 
 
@@ -16,7 +15,7 @@ tmpvcf = 'C:/Users/laure/Documents/BIOL5131/Altai_1000lines.vcf'
 ### update to specific call rather than full df for counts
 #############
 
-subset = allel.read_vcf(tmpvcf, fields = ['variants/REF', 'variants/ALT', 'variants/POS'])
+subset = allel.read_vcf(longvcf, fields = ['variants/REF', 'variants/ALT', 'variants/POS'])
 
 alt_alleles = subset['variants/ALT']
 ref_alleles = subset['variants/REF']
@@ -48,22 +47,24 @@ countdf = pd.DataFrame.from_dict(combinationcounts, orient= "index", columns=["c
 countdf.index =  pd.MultiIndex.from_tuples(countdf.index, names=["REF", "ALT"])
 table = countdf["count"].unstack(fill_value=0)
 
-# table.to_csv('tmp_val.csv', index=False) ##turned off so we don't get too much output, turn back on for final!
+table.to_csv('long_val.csv', index=False) ##turned off so we don't get too much output, turn back on for final!
 
 ####print results in table for bp differences
 pos_df = pd.DataFrame.from_dict(varposcounts, orient="index", columns=["num_differences"])
 pos_df.index.name = "POS"
 # pos_df = pos_df.sort_index()
-pos_df['binned_pos'] = (pos_df.index // 10) * 10 ###update to 100k for final run
+pos_df['binned_pos'] = (pos_df.index // 100000) * 100000
 binned = pos_df.groupby("binned_pos")["num_differences"].sum().reset_index()
+binned.to_csv('long_pos.csv', index = False) ##turned off so we don't get too much output, turn back on for final!
 
-print("\nDifferences per position:")
+print("\nDifferences per position by 100k:")
 print(pos_df)
 
 plt.figure()
-plt.bar(binned["binned_pos"], binned["num_differences"])
+plt.bar(binned["binned_pos"], binned["num_differences"], width=80000, align='center')
 plt.xlabel("Pos")
 plt.ylabel("Diff")
 plt.title("Vart Diff/Pos")
 plt.show()
-plt.savefig('tmp_val.png') ##turned off so we don't get too much output, turn back on for final!
+plt.ylim(0, max(2, binned["num_differences"].max() * 1.1))
+plt.savefig('long_val.png') ##turned off so we don't get too much output, turn back on for final!
